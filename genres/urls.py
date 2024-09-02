ce49d63b36d0750ae048cdad5458c45b3d921d4a
Mateus Luiz de Oliveira Souza-MATEUSLZ <mateuslz@motorola.com>
2024-09-01 23:33:46 -0400

    GenreRetrieveUpdateDestroy View

--- genres/urls.py
+++ genres/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from genres.views import GenreCreateListView, genre_detail_view
+from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
     path('', GenreCreateListView.as_view(), name='genres-create-list'),
-    path('<int:pk>/', genre_detail_view, name='genre-detail-view'),
+    path('<int:pk>/', GenreRetrieveUpdateDestroyAPIView.as_view(), name='genre-detail-view'),
 ]
--- genres/views.py
+++ genres/views.py
@@ -1,7 +1,3 @@
-import json
-from django.http import JsonResponse
-from django.shortcuts import get_object_or_404
-from django.views.decorators.csrf import csrf_exempt
 from genres.models import Genre
 from rest_framework import generics
 from .serializers import GenreSerializer
@@ -12,25 +8,6 @@ class GenreCreateListView(generics.ListCreateAPIView):
     serializer_class = GenreSerializer
 
 
-@csrf_exempt
-def genre_detail_view(request, pk):
-    genre = get_object_or_404(Genre, pk=pk)
-
-    if request.method == 'GET':
-        data = {'id': genre.id, 'name': genre.name}
-        return JsonResponse(
-            data, safe=False
-        )
-    elif request.method == 'PUT':
-        data = json.loads(request.body.decode('utf-8'))
-        genre.name = data['name']
-        genre.save()
-        return JsonResponse(
-            data, safe=False
-        )
-    elif request.method == 'DELETE':
-        genre.delete()
-        return JsonResponse(
-            {'message': 'Gender removed sucessfully'},
-            status=204,
-        )
+class GenreRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
