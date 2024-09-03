7c9b7a285a4cb64ade6354e775b614e8c60666af
Mateus Luiz de Oliveira Souza-MATEUSLZ <mateuslz@motorola.com>
2024-09-02 23:06:02 -0400

    Mean of rate in movies

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,18 +1,28 @@
+from django.db.models import Avg
 from .models import Movie
 from rest_framework import serializers
 
 
 class MovieSerializer(serializers.ModelSerializer):
 
+    rate = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Movie
         fields = "__all__"
 
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+
+        if rate:
+            return round(rate, 1)
+
     def validate_release_date(self, value):
-        if value.year < 1990:
-            raise serializers.ValidationError('The release date cant be lower then 1990')
+        if value.year < 1920:
+            raise serializers.ValidationError('The release date cant be lower then 1920')
         return value
 
     def validate_resume(self, value):
-        if len(value.resume) > 200:
-            raise serializers.ValidationError('Resume cant have more than 200 characters')
+        if len(value) > 400:
+            raise serializers.ValidationError('Resume cant have more than 400 characters')
+        return value
