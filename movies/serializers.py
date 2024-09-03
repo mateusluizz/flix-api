c0c3605f9c398aaf20e73701eb11fb6f71864424
Mateus Luiz de Oliveira Souza-MATEUSLZ <mateuslz@motorola.com>
2024-09-02 22:20:22 -0400

    Validations in movies

--- movies/serializers.py
+++ movies/serializers.py
@@ -7,3 +7,12 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = "__all__"
+
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise serializers.ValidationError('The release date cant be lower then 1990')
+        return value
+
+    def validate_resume(self, value):
+        if len(value.resume) > 200:
+            raise serializers.ValidationError('Resume cant have more than 200 characters')
