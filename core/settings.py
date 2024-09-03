b5d28c5680c7d06a9433671a2e3c053dc84aa658
Mateus Luiz de Oliveira Souza-MATEUSLZ <mateuslz@motorola.com>
2024-09-03 00:18:42 -0400

    JWT Token

--- core/settings.py
+++ core/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
+    'rest_framework_simplejwt',
 
     # My app
+    'authentication',
     'genres',
     'actors',
     'movies',
@@ -128,3 +131,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,8 @@ from django.urls import include, path
 api_v1 = 'api/v1/'
 
 urlpatterns = [
-    path(f'{api_v1}admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path(f'{api_v1}authentication/', include('authentication.urls')),
     path(f'{api_v1}genres/', include('genres.urls')),
     path(f'{api_v1}actors/', include('actors.urls')),
     path(f'{api_v1}movies/', include('movies.urls')),
